702172215309d6cb76276c27622c2a47422ea4c7
cheengguo <cheengguo@tencent.com>
2026-03-13 15:28:39 +0800

    Update version to R02C02

--- src/mpcomm.cpp
+++ src/mpcomm.cpp
@@ -995,7 +995,7 @@ int MPComm::Impl::init(const std::string &local_host_id,
     // Generate MPComm version config file
     {
         namespace fs = std::filesystem;
-        std::string MPCOMM_VERSION = "R01C01";
+        std::string MPCOMM_VERSION = "R01C02";
         std::string platform = "cuda";
         std::string target_dir = "/dockerdata/.trmt/";
         fs::path full_path = fs::path(target_dir) / "mpcomm.config.json";
